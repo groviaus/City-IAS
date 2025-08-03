4522bf6ee85137a4705937cbf6ca0fc2a9ef9d4e
DanishCodebase <ammarsaifi5376@gmail.com>
2025-08-03 23:03:52 +0530

    Update Important Dates component with new event details and styling enhancements
    - Modified event dates and years for accuracy.
    - Added background and text color properties for improved visual consistency.
    - Updated class names in the Card component for better styling management.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -268,7 +268,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en-IN">
+    <html lang="en-IN" suppressHydrationWarning>
       <head>
         {/* Preconnect to improve performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
